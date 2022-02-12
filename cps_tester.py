31ba54dffda4a17d32ec6ddcdc4ed3cbbd1e9ef9
Lawrence Wang <lawrence.anyang.wang@gmail.com>
2022-02-12 15:32:46 -0500

    wip

--- cps_tester.py
+++ cps_tester.py
@@ -1,10 +1,16 @@
+import os
+import pathlib
 from tkinter import *
+import sqlite3
 
 
 class Window(Frame):
+    """setup for the window"""
     def __init__(self, master=None):
         Frame.__init__(self, master)
         self.master = master
+
+        self.initialize()
 
         self.score = "0"
         self.first_time = True
@@ -24,7 +30,35 @@ class Window(Frame):
         self.timer = Label(self, text="0 seconds")
         self.timer.place(x=225, y=25)
 
+    def initialize(self):
+        """start the program"""
+        self.dbpath = os.path.join(pathlib.Path.home(), "cpstester.db")
+        self.con = sqlite3.connect(self.dbpath)
+        self.cursor = self.con.cursor()
+
+        self.cursor.execute("""
+        create table if not exists highscore([highscore] integer)
+        """)
+
+        length_of_highscore = self.cursor.execute(
+            """
+            select count (*) from highscore
+            """
+        )
+
+        length_of_highscore = length_of_highscore.fetchall()[0][0]
+
+        if length_of_highscore == 0:
+            self.cursor.execute(
+                """
+                insert into highscore (highscore) values (0);
+                """
+            )
+
+        self.con.commit()
+
     def start_test(self):
+        """start the test"""
         self.clicking_button.configure(state="disabled")
         self.clicking_button.pack()
         self.clicking_button.place(x=125, y=200)
